7998d131918de3f253cab6ca50e77dff2a689936
DESKTOP-QV3847N\UserK <dlsdk0601@gmail.com>
2021-12-28 00:53:41 +0900

    socket chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,51 @@
 
+const messageList = document.querySelector("ul"); 
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 
-const url = window.location.host;
+const socket = new WebSocket(`ws://${window.location.host}`); 
 
-const socket = new WebSocket(`ws://${url}`); 
 
-alert("성공");
-console.log("adfasfd")
+const makeMessage = (type, payload) => {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("connected to Browser");
+}); //서버와 연결이 시작 됐을떄 
+
+socket.addEventListener("message", (message) => {
+    console.log("new message: ", message.data);
+
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+
+});// 서버에서 message를 받았을떄 
+
+socket.addEventListener("close", () => {
+    console.log("connected from Server");
+}); // 서버와의 연결이 끊겼을떄 
+
+
+setTimeout(()=> {
+    socket.send("hello from the browser");
+}, 10000);
+
+function handleSubmit(e){
+    e.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_massage", input.value));
+    input.value = "";
+}
+
+const handleNickSubmit = (e) => {
+    e.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/pulic", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
@@ -22,11 +22,26 @@ const wss = new WebSocket.Server({server});
 
 //이렇게 두가지 안해도됨. 소켓만 필요하면 소켓만 해도 무방
 
-const handleConnection = (socket) => {  
-    console.log(socket)
-}
+const sockets = []; //fake db 서버에 연결된 누군가를 여기에 넣을거임 
 
-wss.on("connection", handleConnection)
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+
+    socket.on("close", () => {
+        console.log("disconnected from the Browser")
+    }); //프론트가 브라우저를 꺼버렸을 경우
+
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+        sockets.forEach(aSocket => aSocket.send(message.toString("utf8")));
+        //처음에 접속한 브라우저에게 보내는 방법
+
+        
+        // socket.send(message.toString('utf8'));
+    }) // 프론트에서 보낸 message 받기 
+
+    //socket.send("hello");  프론트로 data 보내기 
+})
 // addEventListener와 흡사한 형태. connection이 됐을때, 콜백 함수 실행 
 
 server.listen(3000, handleListen)
