b352c4e6bb5fe0e58cfee1013d7043559ae9f9fa
DESKTOP-QV3847N\UserK <dlsdk0601@gmail.com>
2021-12-31 22:59:57 +0900

    get media Data

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,101 +1,217 @@
 const socket = io();
 //io는 아마도 socket 설치하고 생긴 함수(내장된 함수 일수도.) 이 함수를 통해서 백엔드와 연결시켜준다 
 
-const welcome = document.querySelector("#welcome");
-const room = document.querySelector("#room");
-const form = welcome.querySelector("form");
-const roomList = document.querySelector("#roomList ul");
 
-room.hidden = true;  // 처음에 채팅방 숨기기 
+//------------------video call
 
-let roomname;
+const myFace = document.querySelector("#myFace");
+const mute = document.querySelector("#mute");
+const camera = document.querySelector("#camera");
+const camerasSelect = document.querySelector("#cameras");
 
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.append(li);
-}
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-function handleMessageSubmit(e){
-    e.preventDefault();
-    const input = room.querySelector("#msg input");
-    let value = input.value;
-    socket.emit("new_message", value, roomname, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
+async function getCameras(){ //어떤 카메라를 쓸지 생성하는 함수
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        //유저가 가지고 있는 모든 메디아 정보
+        
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        //디바이스 중에서 카메라 정보만 가져오기
 
-function handleNicknameSubmit(e){
-    e.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname", input.value);
-}
+        const currentCamera = myStream.getVideoTracks()[0];
+        //현재 켜져있는 카메라
 
-function showRoom(nickname){
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomname}`;
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label == camera.label){
+                option.selected = true;
+                //현재 선택된 카메라가 셀렉트 박스 젤 위에 보이게
+            }
+            camerasSelect.appendChild(option);
+        })
 
-    addMessage(`${nickname} joined`);
-    
-
-    const megForm = room.querySelector("#msg");
-    // const nameForm = room.querySelector("#name");
-    megForm.addEventListener("submit", handleMessageSubmit);
-    // nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(e){
-    e.preventDefault();
-    const roominput = form.querySelector("input#roomname");
-    const nickname = form.querySelector("input#name");
-    socket.emit("enter_room", roominput.value, nickname.value, showRoom);
-    
-    roomname = roominput.value;
-    // socket.emit("enter_room", input.value, (msg) => {
-    //     console.log("server done" + msg)
-    // })
-    //websocket에서 send와 비슷한 느낌
-    //emit으로 어떠한 이벤트를 할수있고, 이름도 마음대로 지으면 됨 websocket처럼 clsoe, message 등등 안해도됨 
-    //파라미터를 보낼수 있는데, websocket처럼 객체를 stringify할 필요없이바로 보내진다. 
-    // emit의 인자로 콜백 함수 넣을수 있음, 어떠한 파라미터든 몇개든 넣을수 있지만 콜백함수는 제일 마지막에 넣는다 
-    roominput.value = "";
-    nickname.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomname} (${newCount})`;
-    addMessage(`${user} joined`);
-})
-//welcome이라는 이벤트를 받았을때 실행. 
-
-socket.on("bye", (left, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomname} (${newCount})`;
-     addMessage(`${left} left`)
-})
-// socket.on("new_message", (msg) => addMessage(msg)) 밑에랑 동일함
-socket.on("new_message", addMessage)
-
-socket.on("room_change", (rooms) => { 
-    roomList.innerText = "";
-    
-    if(rooms.length === 0){  //채팅방이 없다면 멈춰
-        return;
+    }catch(e){
+        console.log(e)
     }
+}
+
+async function getMedia(deviceId){
+    const initioalConstrains = {
+        audio: true,
+        video: { facingMode: "user"},  //브라우저든 모바일이든 무조건 셀카 카메라로 작동
+    }
+    const cameraConstraints = {
+        audio: true,
+        video: {
+            deviedId: {exact: deviceId }  //exact 없이 적어주면 해당 아이디값의 카메라가 없을떄, 비디오가 실행 안된다. 
+            //근데 위와 같이 적으면 카메라 찾다가 못찾으면 다른 카메라 실행
+        }
+    }
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initioalConstrains
+        )        
+        // myStream 이라는 변수에 현재 가지고 있는 미디어 장치에 접근하는 방식
+        // 가지고 오고싶은 정보를 객체 형식으로 파라미터로 넣어준다
+        myFace.srcObject = myStream;
+        if(!deviceId){
+            await getCameras();
+            //젤 처음만 실행하면됨. 카메라 변경할때마다 실행되면 selectbox 길어짐
+        }
+    } catch(e){
+        console.log(e);
+    }
+}
+
+getMedia();
+
+
+function handlemuteClick(){
+    myStream.getAudioTracks().forEach( track => track.enabled = !track.enabled);
+    //오디오 동작을 false 시켜줌
+    //트랙을 가져와서 해당 트랙의 enabled 값을 바꿔주면 비활성화가 된다
+
+    if(!muted){
+        mute.innerText = "Unmute";
+        muted = true;
+    }else{
+        muted = false;
+        mute.innerText = "Mute";
+    }
+}
+
+
+function handlecameraClick(){
+    myStream.getVideoTracks().forEach(track =>  track.enabled = !track.enabled);
+    if(!cameraOff){
+        camera.innerText = "Turn Camera Off";
+        cameraOff = true;
+    }else{
+        cameraOff = false;
+        camera.innerText = "Turn Camera On";
+    }
+}
+
+async function handleCameraChange(){
+    await getMedia(camerasSelect.value); 
+}
+
+mute.addEventListener("click", handlemuteClick);
+camera.addEventListener("click", handlecameraClick);
+camerasSelect.addEventListener("input", handleCameraChange); 
+
+//--------------------------with socket.io
+// const welcome = document.querySelector("#welcome");
+// const room = document.querySelector("#room");
+// const form = welcome.querySelector("form");
+// const roomList = document.querySelector("#roomList ul");
+
+// room.hidden = true;  // 처음에 채팅방 숨기기 
+
+// let roomname;
+
+// function addMessage(message){
+//     const ul = room.querySelector("ul");
+//     const li = document.createElement("li");
+//     li.innerText = message;
+//     ul.append(li);
+// }
+
+// function handleMessageSubmit(e){
+//     e.preventDefault();
+//     const input = room.querySelector("#msg input");
+//     let value = input.value;
+//     socket.emit("new_message", value, roomname, () => {
+//         addMessage(`You: ${value}`);
+//     });
+//     input.value = "";
+// }
+
+// function handleNicknameSubmit(e){
+//     e.preventDefault();
+//     const input = room.querySelector("#name input");
+//     socket.emit("nickname", input.value);
+// }
+
+// function showRoom(nickname){
+//     welcome.hidden = true;
+//     room.hidden = false;
+//     const h3 = room.querySelector("h3");
+//     h3.innerText = `Room ${roomname}`;
+
+//     addMessage(`${nickname} joined`);
     
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    })
-});
+
+//     const megForm = room.querySelector("#msg");
+//     // const nameForm = room.querySelector("#name");
+//     megForm.addEventListener("submit", handleMessageSubmit);
+//     // nameForm.addEventListener("submit", handleNicknameSubmit);
+// }
+
+// function handleRoomSubmit(e){
+//     e.preventDefault();
+//     const roominput = form.querySelector("input#roomname");
+//     const nickname = form.querySelector("input#name");
+//     socket.emit("enter_room", roominput.value, nickname.value, showRoom);
+    
+//     roomname = roominput.value;
+//     // socket.emit("enter_room", input.value, (msg) => {
+//     //     console.log("server done" + msg)
+//     // })
+//     //websocket에서 send와 비슷한 느낌
+//     //emit으로 어떠한 이벤트를 할수있고, 이름도 마음대로 지으면 됨 websocket처럼 clsoe, message 등등 안해도됨 
+//     //파라미터를 보낼수 있는데, websocket처럼 객체를 stringify할 필요없이바로 보내진다. 
+//     // emit의 인자로 콜백 함수 넣을수 있음, 어떠한 파라미터든 몇개든 넣을수 있지만 콜백함수는 제일 마지막에 넣는다 
+//     roominput.value = "";
+//     nickname.value = "";
+// }
+
+// form.addEventListener("submit", handleRoomSubmit);
+
+// socket.on("welcome", (user, newCount) => {
+//     const h3 = room.querySelector("h3");
+//     h3.innerText = `Room ${roomname} (${newCount})`;
+//     addMessage(`${user} joined`);
+// })
+// //welcome이라는 이벤트를 받았을때 실행. 
+
+// socket.on("bye", (left, newCount) => {
+//     const h3 = room.querySelector("h3");
+//     h3.innerText = `Room ${roomname} (${newCount})`;
+//      addMessage(`${left} left`)
+// })
+// // socket.on("new_message", (msg) => addMessage(msg)) 밑에랑 동일함
+// socket.on("new_message", addMessage)
+
+// socket.on("room_change", (rooms) => { 
+//     roomList.innerText = "";
+    
+//     if(rooms.length === 0){  //채팅방이 없다면 멈춰
+//         return;
+//     }
+    
+//     rooms.forEach(room => {
+//         const li = document.createElement("li");
+//         li.innerText = room;
+//         roomList.append(li);
+//     })
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 
 // -------------------------websocket 버전
--- src/server.js
+++ src/server.js
@@ -11,22 +11,8 @@ const app = express();
 const server = http.createServer(app);
 //http 서버
 
-// const wss = new WebSocket.Server({server});
-//websocket 서버
+const wsServer = new Server(server);
 
-//이렇게 두가지 안해도됨. 소켓만 필요하면 소켓만 해도 무방
-
-//socket.io 버전
-const wsServer = new Server(server, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    }
-}); //어드민 페이지 보기위한 셋팅
- 
-instrument(wsServer, {
-    auth: false
-}) //어드민 페이지 보안 설정 => 설정하면 아이디비밀번호 설정 가능
 
 
 app.set("view engine", "pug");
@@ -39,82 +25,116 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 //이렇게 작성하는거는 일반 브라우저 방식, 소켓은 ws프로토콜로 작업해야함
 
+//------------------video call
+
+
+
+
+
+
+
+
+
+
+
 
 //---------------------------socket.io 버전
 
-function publicRooms(){
-    const { sockets: {adapter: { sids, rooms } } } = wsServer
-    //ws 서버에 adapter라는 기능이 있음 => 엄청 큰 앱은 하나의 db로 모든 유저를 다루지 않는다
-    //같은 프론트단을 보더라도 다른 서버를 이용 할 수도있음 
-    //이론상 다른 서버에 있는 유저끼리는 정보를 주고받고 못하지만 adapter가 해준다
+//socket.io 버전
+// const wsServer = new Server(server, {
+//     cors: {
+//         origin: ["https://admin.socket.io"],
+//         credentials: true
+//     }
+// }); //어드민 페이지 보기위한 셋팅
+ 
+// instrument(wsServer, {
+//     auth: false
+// }) //어드민 페이지 보안 설정 => 설정하면 아이디비밀번호 설정 가능
 
 
-    // const sids = wsServer.sockets.adapter.sids;
-    // const rooms = wsServer.sockets.adapter.rooms;
-
-    const publicRooms = [];
-    rooms.forEach( (_, key) => {
-        if(sids.get(key) === undefined) { //privite room을 찾아줌
-            publicRooms.push(key);
-
-        }
-    })
+// function publicRooms(){
+//     const { sockets: {adapter: { sids, rooms } } } = wsServer
+//     //ws 서버에 adapter라는 기능이 있음 => 엄청 큰 앱은 하나의 db로 모든 유저를 다루지 않는다
+//     //같은 프론트단을 보더라도 다른 서버를 이용 할 수도있음 
+//     //이론상 다른 서버에 있는 유저끼리는 정보를 주고받고 못하지만 adapter가 해준다
 
 
-    return publicRooms;
-}
+//     // const sids = wsServer.sockets.adapter.sids;
+//     // const rooms = wsServer.sockets.adapter.rooms;
 
-function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
+//     const publicRooms = [];
+//     rooms.forEach( (_, key) => {
+//         if(sids.get(key) === undefined) { //privite room을 찾아줌
+//             publicRooms.push(key);
+
+//         }
+//     })
 
 
-wsServer.on("connection", socket => {
-    socket["nickname"] = "someone";
-    socket.onAny((e) => {
-        console.log(`socket event : ${e}`);
-        //미들웨어같이 어디서든 콘솔 찍어줌
-    });
+//     return publicRooms;
+// }
 
-    socket.on("enter_room", (roominput, nickname, done) => {
+// function countRoom(roomName){
+//     return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+// }
+
+
+// wsServer.on("connection", socket => {
+//     socket["nickname"] = "someone";
+//     socket.onAny((e) => {
+//         console.log(`socket event : ${e}`);
+//         //미들웨어같이 어디서든 콘솔 찍어줌
+//     });
+
+//     socket.on("enter_room", (roominput, nickname, done) => {
         
-        socket["nickname"] = nickname;
-        socket.join(roominput);
-        done(nickname);
+//         socket["nickname"] = nickname;
+//         socket.join(roominput);
+//         done(nickname);
 
-        socket.to(roominput).emit("welcome", socket.nickname, countRoom(roominput));
-        //방에 잇는 모두에세 메세지 보내기 
-        //welcome 이라는 이벤트를 모두에게 전송
+//         socket.to(roominput).emit("welcome", socket.nickname, countRoom(roominput));
+//         //방에 잇는 모두에세 메세지 보내기 
+//         //welcome 이라는 이벤트를 모두에게 전송
 
 
-        // setTimeout(() => {
-        //     done("hello world")
-            //프론트단에서 보낸 콜백함수 
-            //이건 프론트단에서 실행되는 함수임. 
-            //그런데 콜백함수의 파라미터는 백엔드에서 설정 가능 
-        // }, 5000);
+//         // setTimeout(() => {
+//         //     done("hello world")
+//             //프론트단에서 보낸 콜백함수 
+//             //이건 프론트단에서 실행되는 함수임. 
+//             //그런데 콜백함수의 파라미터는 백엔드에서 설정 가능 
+//         // }, 5000);
 
-        wsServer.sockets.emit("room_change", publicRooms());
-    })
+//         wsServer.sockets.emit("room_change", publicRooms());
+//     })
 
-    socket.on("disconnecting", () => { //방을 떠나기 직전
-        socket.rooms.forEach(room => {
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) -1 ); //본인이 떠나기 직전에 실행되니, 본인 퐇마그래서 -1 해줘야함
-        });
-    })
-    socket.on("disconnect", () => {  //방을 떠난 후 
-        wsServer.sockets.emit("room_change", publicRooms());
-        //방을 떠났기 떄문에 빈 배열 출력
-    })
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    })
-    // socket.on("nickname", (name) => socket["nickname"] = name);
-})
+//     socket.on("disconnecting", () => { //방을 떠나기 직전
+//         socket.rooms.forEach(room => {
+//             socket.to(room).emit("bye", socket.nickname, countRoom(room) -1 ); //본인이 떠나기 직전에 실행되니, 본인 퐇마그래서 -1 해줘야함
+//         });
+//     })
+//     socket.on("disconnect", () => {  //방을 떠난 후 
+//         wsServer.sockets.emit("room_change", publicRooms());
+//         //방을 떠났기 떄문에 빈 배열 출력
+//     })
+//     socket.on("new_message", (msg, room, done) => {
+//         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+//         done();
+//     })
+//     // socket.on("nickname", (name) => socket["nickname"] = name);
+// })
+
+
+
+
 
 
 //---------------------------websocket을 이용한 버전
+
+// const wss = new WebSocket.Server({server});
+//websocket 서버
+
+
 // const sockets = []; 
 // fake db 서버에 연결된 누군가를 여기에 넣을거임 
 
@@ -149,8 +169,6 @@ wsServer.on("connection", socket => {
 // // addEventListener와 흡사한 형태. connection이 됐을때, 콜백 함수 실행 
 
 
-
-//socket.io
 
 
 server.listen(3000, handleListen)
